6055138dde94ccaf4f37055ce7c88be7a6ec9164
bachiriMohamedBaki <bachiri-m@brandsirius.com>
2024-07-18 09:17:31 +0100

    added morgan logger

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv"
 import express from "express"
-
+import morgan from "morgan"
 app.use(express.json())
+app.use(morgan("dev"))
 dotenv.config(
     {
         path: "./config/config.env",
